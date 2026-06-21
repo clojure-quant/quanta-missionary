bdb1df9da2478e907e6d4e70e5d92e3f45f814f1
awb99 <awb99@gmail.com>
2026-06-21 11:45:59 -0500

    logger and time-flow move in

---
+++ src/quanta/missionary/logger.clj
@@ -0,0 +1,64 @@
+(ns quanta.missionary.logger
+  (:require
+   [tick.core :as t]
+   [missionary.core :as m]))
+
+(defn time-buffered [duration-ms flow]
+  (m/ap
+   (let [restartable (second (m/?> (m/group-by {} flow)))]
+     (m/? (->> (m/ap (m/amb= (m/?> restartable)
+                             (m/? (m/sleep duration-ms ::end))))
+               (m/eduction (take-while #(not= % ::end)))
+               (m/reduce conj))))))
+
+(defn merge-events [events]
+  (->> events
+       (map (fn [x] (str "\n" x)))
+       (apply str)))
+
+(defn- write-batch! [filename console? events]
+  (let [s (str "\r\n " (merge-events events))]
+    (when console?
+      (println s))
+    (spit filename s :append true)))
+
+(defn flow-logging-task [filename console? log-f]
+  (let [blocked-f (time-buffered 500 log-f)
+        logging-f (m/ap
+                   (loop []
+                     (m/amb
+                      (let [v (m/?> blocked-f)]
+                        (m/? (m/via m/blk (write-batch! filename console? v)))
+                        v)
+                      (recur))))]
+    (m/reduce (fn [_r _v] nil) nil logging-f)))
+
+(defn start-logging-flow [filename console? log-f]
+  ((flow-logging-task filename console? log-f) prn prn))
+
+(defn create-logger [filename console?]
+  (let [log-a (atom "")
+        log-f (m/watch log-a)]
+    {:dispose! (start-logging-flow filename console? log-f)
+     :log! (fn [t]
+             (reset! log-a t))}))
+
+(defn stop-logger [this]
+  (:dispose! this))
+
+(defn log
+  [this s]
+  ((:log! this) s))
+
+(defn start-log-flow-to-logger [this f]
+  (assert this "start-log-flow-to-logger needs this")
+  (assert f "start-log-flow-to-logger needs f")
+  (let [logging-f (m/ap
+                   (loop []
+                     (m/amb
+                      (let [v (m/?> f)]
+                        (m/? (m/via m/blk (log this v)))
+                        v)
+                      (recur))))
+        log-t (m/reduce (fn [_r _v] nil) nil logging-f)]
+    (log-t #(println "flow-logger done" %) #(println "flow-logger error" %))))
---
+++ src/quanta/missionary/time_flow.clj
@@ -0,0 +1,15 @@
+(ns quanta.missionary.time-flow
+  (:require
+   [missionary.core :as m]))
+
+(defn create-time-flow
+  "returns a missionary flow that fires input orders over time.
+     input is a partition-2 seq.
+     first value of a partition is the next sleep time in secons
+     second falue is a order-action (:type :new-order or :cancel-order)"
+  [time-data-partitions]
+  (let [input (m/seed (partition 2 time-data-partitions))]
+    (m/ap   (let [[sleep-sec msg] (m/?> input)]
+              (when (> sleep-sec 0)
+                (m/? (m/sleep (* 1000 sleep-sec))))
+              msg))))
