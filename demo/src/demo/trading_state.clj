1200f7bfce9ef465ac3404a101bdffc5ae93373f
awb99 <awb99@gmail.com>
2026-06-25 12:59:08 -0500

    time-flow in ms

---
+++ demo/src/demo/trading_state.clj
@@ -0,0 +1,84 @@
+(ns demo.trading-state
+  (:require
+   [missionary.core :as m]
+   [quanta.missionary.time-flow :refer [create-time-flow]]
+   [quanta.missionary :refer [mix mix-tagged]]))
+
+(def trade-f
+  (create-time-flow
+   [0 {:buy  50}
+    0 {:buy  50}
+    1 {:buy  50}
+    0 {:buy 150}
+    5 {:sell 250}
+    0 {:sell  50}
+    4 {:buy 400}]))
+
+(def order-state-f
+  (create-time-flow
+   [0 [{:order/id 1 :side :buy :qty 100 :status :created}]
+    4 [{:order/id 1 :side :buy :qty 100 :status :filled}
+       {:order/id 2 :side :buy :qty 200 :status :created}]
+    2 [{:order/id 2 :side :buy :qty 200 :status :filled}
+       {:order/id 3 :side :buy :qty 300 :status :created}]
+    2 [{:order/id 3 :side :buy :qty 300 :status :filled}
+       {:order/id 4 :side :buy :qty 400 :status :created}]]))
+
+(def tagged-flow
+  (mix-tagged {:trade trade-f
+               :order-state order-state-f}))
+
+(defn acc-state [state [k v]]
+  (case k
+    :trade (update state :trade conj v)
+    :order-state (assoc state :order-state v)))
+
+((->> (m/ap
+       (let [[_ batch] (m/?> (m/group-by {}
+                                         ;(m/ap (m/?> (m/amb= trade-f order-state-f)))
+                                         tagged-flow))]
+         (m/? (->> (m/ap (m/amb= (m/?> batch)
+                                 (m/? (m/sleep 50))))
+                   (m/eduction (take-while some?))
+                   ;(m/reduce conj {:trade []})
+                   (m/reduce acc-state {:trade []})))))
+      (m/eduction (take 20))
+      (m/reduce conj))
+ prn prn)
+
+[{:trade [{:buy 100}],
+  :order-state [{:order/id 1, :side :buy, :qty 100, :status :created}]}
+
+ {:trade [{:buy 200}]}
+
+ {:trade [],
+  :order-state [{:order/id 1, :side :buy, :qty 100, :status :filled}
+                {:order/id 2, :side :buy, :qty 200, :status :created}]}
+
+ {:trade [{:sell 300}]}
+ {:trade [],
+  :order-state [{:order/id 2, :side :buy, :qty 200, :status :filled}
+                {:order/id 3, :side :buy, :qty 300, :status :created}]}
+ {:trade [{:buy 400}]}
+ {:trade [],
+  :order-state [{:order/id 3, :side :buy, :qty 300, :status :filled}
+                {:order/id 4, :side :buy, :qty 400, :status :created}]}]
+
+
+[{:trade [{:buy 50} {:buy 50}], 
+  :order-state [{:order/id 1, :side :buy, :qty 100, :status :created}]} 
+ 
+ {:trade [{:buy 50} {:buy 150}]} 
+ 
+ {:trade [], 
+  :order-state [{:order/id 1, :side :buy, :qty 100, :status :filled} 
+                {:order/id 2, :side :buy, :qty 200, :status :created}]}
+ {:trade [{:sell 250} {:sell 50}], 
+  :order-state [{:order/id 2, :side :buy, :qty 200, :status :filled}
+                {:order/id 3, :side :buy, :qty 300, :status :created}]} 
+ 
+ {:trade [], 
+  :order-state [{:order/id 3, :side :buy, :qty 300, :status :filled} 
+                {:order/id 4, :side :buy, :qty 400, :status :created}]}
+ 
+ {:trade [{:buy 400}]}]
--- src/quanta/missionary/time_flow.clj
+++ src/quanta/missionary/time_flow.clj
@@ -5,11 +5,11 @@
 (defn create-time-flow
   "returns a missionary flow that fires input orders over time.
      input is a partition-2 seq.
-     first value of a partition is the next sleep time in secons
+     first value of a partition is the next sleep time in milli-seconds
      second falue is a order-action (:type :new-order or :cancel-order)"
   [time-data-partitions]
   (let [input (m/seed (partition 2 time-data-partitions))]
     (m/ap   (let [[sleep-sec msg] (m/?> input)]
               (when (> sleep-sec 0)
-                (m/? (m/sleep (* 1000 sleep-sec))))
+                (m/? (m/sleep sleep-sec)))
               msg))))
